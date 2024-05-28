9695a3495beb0682cab23b2dfa0ddb44504d4610
f4tal-err0r <ymekuria6@gmail.com>
2024-05-27 21:38:39 -0400

    Completed ident and refactored start

---
+++ pkgs/discord/ident.go
@@ -0,0 +1,40 @@
+package discord
+
+import (
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/f4tal-err0r/discord_faas/pkgs/config"
+)
+
+func GetCurrentUser(token string) *discordgo.User {
+	session, err := discordgo.New("Bearer " + token)
+	if err != nil {
+		log.Fatalf("Error creating Discord session: %v", err)
+	}
+
+	err = session.Open()
+	if err != nil {
+		log.Fatalf("Error opening Discord session: %v", err)
+	}
+	defer session.Close()
+
+	user, err := session.User("@me")
+	if err != nil {
+		log.Fatalf("Error getting current user: %v", err)
+	}
+	return user
+}
+
+func GetUserGuildInfo(gid string, user *discordgo.User) []string {
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal("ERR: Unable to fetch config: %w", err)
+	}
+	botSession := GetSession(cfg)
+	member, err := botSession.GuildMember(gid, user.ID)
+	if err != nil {
+		log.Fatalf("Error getting guild member: %v", err)
+	}
+	return member.Roles
+}
--- pkgs/discord/start.go
+++ pkgs/discord/start.go
@@ -9,7 +9,7 @@ import (
 	"github.com/f4tal-err0r/discord_faas/pkgs/config"
 )
 
-func StartDiscordBot(cfg *config.Config) {
+func GetSession(cfg *config.Config) *discordgo.Session {
 	dc, err := discordgo.New("Bot " + cfg.Discord.Token)
 	if err != nil {
 		log.Fatalf("ERR: %s", err)
@@ -17,7 +17,16 @@ func StartDiscordBot(cfg *config.Config) {
 	if err := dc.Open(); err != nil {
 		log.Fatal(err)
 	}
+	return dc
+}
 
+func StartDiscordBot() {
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal("ERR: Unable to fetch config: %w", err)
+	}
+
+	dc := GetSession(cfg)
 	log.Println("Bot running....")
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
